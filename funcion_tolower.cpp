49ef447fd6ed830b183b752e5e5f3be7eb6d0bc1
diego zambrana salas <155786560+ELCONECTOR@users.noreply.github.com>
2024-01-15 21:34:19 -0400

    Add files via upload

---
+++ funcion_tolower.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cctype> // Necesario para tolower
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	char caracter;//define la varianle
+	cout<<"ingrese una letra: ";
+	cin>>caracter;//guaarda la variable
+	caracter= tolower(caracter);//funcion para convertir en miniscula
+	switch(caracter)
+	{
+	case 'a':
+	
+	case 'e': 
+	
+	case 'i': 
+	
+	case 'o':
+	
+	case 'u': cout<<"es vocal ";
+	break;
+	default: cout<<"no es vocal";
+	}
+	
+	return 0;
+}
+
---
+++ menú.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+//autor:diego zambrana salas
+//hacer un menú
+//15/1/2024
+/*
+DATOS DE INICIO DEL PROGRAMA
+1:OPCION  UNO
+2:OPCION DOS
+0:SALIR
+*/
+int main(int argc, char *argv[]) {
+	int opcion;
+	opcion=1;
+	do {
+		cout<<"MENU PRINCIAL"<<endl;
+		cout<<"1:OPCION  UNO"<<endl;
+		cout<<"2:OPCION DOS"<<endl;
+		cout<<"0:SALIR"<<endl;
+		cin>>opcion;
+		switch(opcion){
+			case 1:cout<<"Eligio la opcion 1"<<endl; 
+			break;
+			case 2:cout<<"Eligio la opcion 2"<<endl; 
+			break;
+			case 0:cout<<"Eligio la opcion salir"<<endl; 
+			break;
+			default:cout<<"No escogio la opcion correcta"<<endl;
+		}
+	} while(opcion != 0);
+	return 0;
+}
+
