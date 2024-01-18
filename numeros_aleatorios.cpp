fafc38b4c6456ffc173339c02ceccf1c325eb83f
diego zambrana salas <155786560+ELCONECTOR@users.noreply.github.com>
2024-01-17 21:20:48 -0400

    Add files via upload

---
+++ numeros_aleatorios.cpp
@@ -0,0 +1,40 @@
+/*
+Titulo: Generacion de numeros aleatorios. 
+Autor:Diego zambrana salas
+Descripcion: Escribir un programa que imprima numeros aleatorios entre 0 - 99, donde "N" sea ingresado por el usuario. 
+Fecha:17/01/2024
+
+*/
+
+#include <iostream>
+#include <cstdlib> // Necesario para la funcion rand()
+#include <ctime>   // Necesario para la funcion time()
+
+
+using namespace std;
+
+int main() {
+	int cantidad;
+	
+	// Mensaje para solicitar al usuario que ingrese la cantidad de n?meros a generar
+	cout << " Generacion de numeros Aleatorios\n";
+	cout << "¿Cuantos numeros quiere generar? ";
+	cin >> cantidad;
+	
+	// Inicializar la semilla para obtener n?meros realmente aleatorios
+	srand(time(0));
+	
+	// Mostrar los n?meros aleatorios generados
+	cout << "Nnmeros aleatorios generados: ";
+	for (int i = 0; i < cantidad; ++i) {
+		// Generar n?meros aleatorios entre 0 y 99 y mostrarlos
+		cout << rand() % 100;
+		// Agregar una coma y un espacio entre los n?meros (excepto el ?ltimo)
+		if (i < cantidad - 1) {
+			cout << ", ";
+		}
+	}
+	cout << endl;
+	
+	return 0;
+}
---
+++ tabla_de_multiplicaion.cpp
@@ -0,0 +1,29 @@
+/*
+Titulo: Tabla de Multiplicar. 
+Autor:diego zambrana salas
+Descripcion: Escribir un programa que imprima la tabla de multiplicar de un numero dado por el usuario. 
+Fecha:17/01/2024
+*/
+
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+	int numero;
+	
+	// Mensaje para solicitar al usuario que ingrese un n?mero
+	cout << " Tabla de Multiplicar\n";
+	cout << "Ingrese el numero para realizar la tabla: ";
+	cin >> numero;
+	
+	// Mostrar la tabla de multiplicar del n?mero ingresado
+	cout << "Tabla de multiplicar del " << numero << ":\n";
+	for (int i = 1; i <= 10; ++i) {
+		// Mostrar cada multiplicaci?n en una l?nea
+		cout << numero << "x" << i << "=" << (numero * i) << "\n";
+	}
+	
+	return 0;
+}
